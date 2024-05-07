1bf79b906a9741cea647bb0e3528b9ad359c3470
prostiwlsn <prostiwlsn@gmail.com>
2024-05-08 01:25:34 +0700

    Change gitignore, delete BlogService

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
-using Blog;
 using Microsoft.Extensions.Logging;
 
 class Program
@@ -40,7 +39,7 @@ class Program
         */
 
         Console.WriteLine(
-             JsonSerializer.Serialize(BlogService.NumberOfCommentsPerUser(context)));
+             JsonSerializer.Serialize(NumberOfCommentsPerUser(context)));
 
         Console.WriteLine("Posts ordered by date of last comment. Result should include text of last comment:");
         //ToDo: write a query and dump the data to console
@@ -70,7 +69,7 @@ class Program
         */
 
         Console.WriteLine(
-             JsonSerializer.Serialize(BlogService.PostsOrderedByLastCommentDate(context)));
+             JsonSerializer.Serialize(PostsOrderedByLastCommentDate(context)));
 
         Console.WriteLine("How many last comments each user left:");
         // 'last comment' is the latest Comment in each Post
@@ -92,7 +91,7 @@ class Program
         */
 
         Console.WriteLine(
-             JsonSerializer.Serialize(BlogService.NumberOfLastCommentsLeftByUser(context)));
+             JsonSerializer.Serialize(NumberOfLastCommentsLeftByUser(context)));
 
         // Console.WriteLine(
         //     JsonSerializer.Serialize(BlogService.NumberOfCommentsPerUser(context)));
@@ -134,4 +133,41 @@ class Program
         });
         context.SaveChanges();
     }
+    public static List<KeyCount> NumberOfCommentsPerUser(MyDbContext context) => context.BlogComments
+        .GroupBy(comment => comment.UserName)
+        .Select(group => new KeyCount { name = group.Key, count = group.Count() })
+        .ToList();
+
+    public static List<PostLastComment> PostsOrderedByLastCommentDate(MyDbContext context) => context.BlogPosts
+        .Select(post => new {
+            name = post.Title,
+            lastComment = post.Comments.OrderByDescending(comment => comment.CreatedDate).First()
+        })
+        .Select(post => new PostLastComment
+        {
+            name = post.name,
+            lastCommentDate = post.lastComment.CreatedDate,
+            lastCommentText = post.lastComment.Text
+        })
+        .OrderByDescending(post => post.lastCommentDate)
+        .ToList();
+
+    public static List<KeyCount> NumberOfLastCommentsLeftByUser(MyDbContext context) => context.BlogPosts
+        .Select(post => post.Comments
+            .OrderByDescending(comment => comment.CreatedDate).First())
+        .GroupBy(comment => comment.UserName)
+        .Select(group => new KeyCount { name = group.Key, count = group.Count() })
+        .ToList();
+}
+
+public class KeyCount
+{
+    public string name { get; set; }
+    public int count { get; set; }
+}
+public class PostLastComment
+{
+    public string name { get; set; }
+    public DateTime lastCommentDate { get; set; }
+    public string lastCommentText { get; set; }
 }
